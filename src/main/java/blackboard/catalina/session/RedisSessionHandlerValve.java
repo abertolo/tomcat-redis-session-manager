4f662edcaf4a2129c3528dceb478b35fea584706
abertolo <alejandrobertolo@gmail.com>
2014-03-26 18:34:17 -0300

    modificaciones para syi

--- src/main/java/blackboard/catalina/session/RedisSessionHandlerValve.java
+++ src/main/java/blackboard/catalina/session/RedisSessionHandlerValve.java
@@ -9,25 +9,50 @@ import java.io.IOException;
 
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import java.util.regex.Pattern;
 
+public class RedisSessionHandlerValve extends ValveBase {
+	private final Log log = LogFactory.getLog(RedisSessionManager.class);
 
-public class RedisSessionHandlerValve extends ValveBase
-{
-  private final Log log = LogFactory.getLog( RedisSessionManager.class );
+	protected String uriExclude = "/ping/.*";
 
-  @Override
-  public void invoke( Request request, Response response ) throws IOException, ServletException
-  {
-    RedisSessionManager manager = (RedisSessionManager) getContainer().getManager();
+	private Pattern uriExcludePattern;
 
-    try
-    {
-      manager.beforeRequest( request );
-      getNext().invoke( request, response );
-    } finally
-    {
-      manager.afterRequest( request );
-    }
-  }
+	@Override
+	public void invoke(Request request, Response response) throws IOException, ServletException {
+		RedisSessionManager manager = (RedisSessionManager) getContainer().getManager();
+		String fullUri = getRequestFullUri(request);
+		boolean exclude = getUriExcludePattern().matcher(fullUri).matches();
+		try {
+			if(!exclude)
+				manager.beforeRequest(request, fullUri);
+			
+			getNext().invoke(request, response);
+
+		} finally {
+			if(!exclude)
+				manager.afterRequest(request);
+		}
+	}
+
+	public String getRequestFullUri(Request request){
+		return request.getRequestURI() + (request.getQueryString() != null ? "?" + request.getQueryString() : "");
+	}
+
+	public String getUriExclude() {
+		return uriExclude;
+	}
+
+	public void setUriExclude(String ue) {
+		this.uriExclude = ue;
+	}
+
+	private Pattern getUriExcludePattern(){
+		if(uriExcludePattern == null){
+			uriExcludePattern = Pattern.compile(getUriExclude());
+			log.info("getUriExcludePattern compile regex: " + getUriExclude());
+		}
+		return uriExcludePattern;
+	}
 
 }
